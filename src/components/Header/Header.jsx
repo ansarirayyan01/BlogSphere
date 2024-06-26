c040880b9edcf9e605b3a2853cd65ad3f501078e
ansarirayyan01 <ansarirayyan874@gmail.com>
2024-06-26 15:47:18 +0530

    fix the style in header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,7 +40,7 @@ function Header() {
   return (
     <header className='py-3 px-3 shadow bg-gray-500 text-white'>
       {/* <Container> */}
-        <nav className='flex'>
+        <nav className='flex '>
           <div className='mr-4'>
             <Link to='/'>
               <Logo width='70px'   />
@@ -53,7 +53,7 @@ function Header() {
               <li key={item.name}>
                 <button
                 onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-gray-700 rounded-full'
+                className='inline-bock px-4 py-2 duration-200 hover:bg-gray-700 rounded-full'
                 >{item.name}</button>
               </li>
             ) : null
